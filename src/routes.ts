3503bf42c94354bf52c45cd3ef18f89a1ec4e625
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-08 04:54:02 -0300

    feat: importando o Prisma e o Submite dentro do meu arquivo routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import nodemailer from 'nodemailer';
 import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router()
 
@@ -15,6 +17,17 @@ const transport = nodemailer.createTransport({
 
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  )
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
 
   await transport.sendMail({
       from: 'Equipe Feedget <oi@oi.com>',
