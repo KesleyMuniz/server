6905e7d918a6c2a9475e9b810dc6edb10603b756
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-08 04:21:53 -0300

    feat: rotas separadas do arquivo server

---
+++ src/routes.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import nodemailer from 'nodemailer';
+import { prisma } from "./prisma";
+
+export const routes = express.Router()
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "69aa8b4cb1401e",
+    pass: "b8f9cd8c7f8eb0"
+  }
+});
+
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+      data: {
+          type,
+          comment,
+          screenshot,
+      }
+  });
+
+  await transport.sendMail({
+      from: 'Equipe Feedget <oi@oi.com>',
+      to: 'Kesley <skyzone121@hotmail.com>',
+      subject: 'Novo feedback',
+      html: [
+          `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+          `<p>Tipo do feedback: ${type}</p>`,
+          `<p>Comentário: ${comment}</p>`,
+          `</div>`
+      ].join('\n')
+  })
+  
+
+  return res.status(201).json({data: feedback});
+})
